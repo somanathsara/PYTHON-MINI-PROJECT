d852b18e910c2861f486e64689ecc6d7a7d1bdb7
somanathsara <somanathsaras@gmail.com>
2026-07-21 17:50:03 +0530

    with '#' will be ignored, and an empty message aborts the commit.

--- test2.py
+++ test2.py
@@ -1,4 +1,4 @@
-# data = [{"name":"ram","age":12},{"name":"shyam","age":5}]
-# for i in range(len(data)):
-#     print(data[i]["name"])
-#     print(data[i]["age"])
+data = [{"name":"ram","age":12},{"name":"shyam","age":5}]
+for i in range(len(data)):
+    print(data[i]["name"])
+    print(data[i]["age"])
